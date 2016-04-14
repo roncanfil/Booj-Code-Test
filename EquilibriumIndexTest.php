33670cad76c5c25de47f6de013327fbbc043bae2
ronhenry <ronald.henriquez@gmail.com>
2016-04-14 12:42:39 -0600

    Length of commentaries was reduced

--- EquilibriumIndexTest.php
+++ EquilibriumIndexTest.php
@@ -2,11 +2,18 @@
 
 /*
 
-My first thought was "divide and conquer" to divide the array in the middle into two subarrays (left and right) and compare the total sum of their elements, but since I need to assume that "the sequence may be very long" the order of that code will be O(n^2).
+My first thought was "divide and conquer" to divide the array in the middle into two subarrays 
+(left and right) and compare the total sum of their elements, but since I need to assume that 
+"the sequence may be very long" the order of that code will be O(n^2).
 
-Just like with some sort algorithms, I can start from one of the edges of the array to try to find the equilibrium indexes with an order O(n). 
+Just like with some sort algorithms, I can start from one of the edges of the array to try to 
+find the equilibrium indexes with an order O(n). 
 
-To do that, I'll start assuming the total sum of the left array ($left) is 0 and I'll compare it to the total sum of the elements of the right array ($right). If they match, it means that the element is an index. I keep doing this considering the new "$left" as the the sum of its previous value plus the new element and the new "$right" as the subtraction of the new element to its current value. Since I'm going to be working with arrays, I'll store the indexes in an array to display the results.
+To do that, I'll start assuming the total sum of the left array ($left) is 0 and I'll compare it 
+to the total sum of the elements of the right array ($right). If they match, it means that the element
+is an index. I keep doing this considering the new "$left" as the the sum of its previous value plus 
+the new element and the new "$right" as the subtraction of the new element to its current value. 
+Since I'm going to be working with arrays, I'll store the indexes in an array to display the results.
 
 */
  
